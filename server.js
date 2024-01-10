fab17caa9a63ba1f19f1f1eb75e7084d71c35dc2
GOATNINJA10 <148058898+KshitijBramhecha@users.noreply.github.com>
2024-01-10 23:41:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const path = require('path')
 const connectDb = require('./config/connectDb')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 
 //config dot env file
 dotenv.config();
@@ -28,6 +21,13 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 //routes
 /*app.use(
